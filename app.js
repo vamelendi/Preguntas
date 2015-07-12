9d1d7c3ac08b5e04fbb5942d1c3191c4d5bcf031
Vitor Alonso <vamelendi@mimail.com>
2015-07-12 08:58:09 +0200

    Marco de la aplicacion

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
 var routes = require('./routes/index');
 
 var app = express();
+
+app.use(partials());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
