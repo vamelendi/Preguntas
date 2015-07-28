c46e0a05b5492c603f56b714083eba70d286f024
Vitor Alonso <vamelendi@mimail.com>
2015-07-28 22:28:24 +0200

    adaptacion a postgres para despeglar en heroku

--- models/models.js
+++ models/models.js
@@ -1,9 +1,31 @@
 var path = require('path');
+//Configurar bases de datos postgres y sqlite
+//formato de urls
+// Postgres: postgres://user:passwd@host:port/database
+//SQLite: sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user    = (url[2]||null);
+var pwd	    = (url[3]||null);
+var protocol= (url[1]||null);
+var dialect = (url[1]||null);
+var port    = (url[5]||null);
+var host    = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-//Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null, {dialect:"sqlite", storage:"quiz.sqlite"});
+//Usar BBDD SQLite o postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+     {dialect:  dialect,
+      protocol: protocol,
+      port:     port,
+      host:     host,
+      storage:  storage,  //solo sqlite
+      omitNull: true   //solo postgress
+    });
 
 //Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
