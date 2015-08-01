bab590aa9a15b3c2f7993081416b641721510b23
Vitor Alonso <vamelendi@mimail.com>
2015-08-01 10:11:39 +0200

    Crear Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,12 +12,14 @@ exports.load = function(req, res, next, quizId){
 	).catch(function(error) {next(error);});	
 };
 
+
 //GET/quizes
 exports.index=function(req,res){
 	models.Quiz.findAll().then(function(quizes){
 	  res.render('quizes/index', {quizes: quizes});
 	}).catch(function (error) {next(error);})
 };
+
 
 
 //GET /quizes/:id
@@ -34,4 +36,20 @@ exports.answer = function(req,res){
 	   resultado = 'Correcto';
 	} 
              res.render('quizes/answer',{quiz:req.quiz, respuesta: resultado});    			
+};
+
+//GET /quizes/new
+exports.new =function(req, res){
+	var quiz= models.Quiz.build(
+	{pregunta: "Pregunta", respuesta: "Respuesta"});
+	res.render('quizes/new', {quiz:quiz});
+};
+
+//POST /quizes/create
+exports.create =function(req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
+	//guarda eN BD los campos pregunta y respuesta de quiz
+	quiz.save({fields:["pregunta","respuesta"]}).then(function(){res.redirect('/quizes')
+	//res.redirect: Redireccion HTTP a lista de preguntas
+});
 };
